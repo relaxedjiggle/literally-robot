7cf278f0c35480fe3a75f6a35e1ce0f771e58908
Siegmar Durward <167298792+relaxedjiggle@users.noreply.github.com>
2024-04-17 12:02:08 +0300

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,36 @@
+bool SaveTest(const std::string& filename, bool append,
+  const Paths64* subj, const Paths64* subj_open, const Paths64* clip, 
+  int64_t area, int64_t count, ClipType ct, FillRule fr)
+{
+  string line;
+  bool found = false;
+  int last_cap_pos = 0, curr_cap_pos = 0;
+  int64_t last_test_no = 0;
+  if (append && FileExists(filename)) //get the number of the preceeding test
+  {
+    ifstream file;
+    file.open(filename, std::ios::binary);
+    if (!file || !file.good()) return false;
+    BMH_Search bmh = BMH_Search(file, "CAPTION:");
+    while (bmh.FindNext()) ;
+    if (bmh.LastFound())
+    {
+      line.assign(bmh.LastFound()+8, bmh.FindNextEndLine());
+      string::const_iterator s_it = line.cbegin(), s_end = line.cend();
+      GetInt(s_it, s_end, last_test_no);
+    }
+  } 
+  else if (FileExists(filename)) 
+    remove(filename.c_str());
+
+  ++last_test_no;
+
+  std::ofstream source;
+  if (append && FileExists(filename))
+    source.open(filename, ios_base::app | ios_base::ate);
+  else
+    source.open(filename);
+
+  string cliptype_string;
+  switch (ct)
+  {
